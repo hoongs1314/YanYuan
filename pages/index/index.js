37e25cdca75cc4e9962a3b848cfa9c189963689d
HY <2779692557@qq.com>
2019-04-28 15:24:55 +0800

    HY
    首页轮播图，首页轮播公告

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,67 +7,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls:[
-      { "url":"../images/food/1.jpg"},
-      { "url":"../images/food/2.jpg"},
-      { "url":"../images/food/3.jpg"},
-      { "url":"../images/food/4.jpg"}
+    imgUrls: [{
+        "url": "../images/food/1.jpg"
+      },
+      {
+        "url": "../images/food/2.jpg"
+      },
+      {
+        "url": "../images/food/3.jpg"
+      },
+      {
+        "url": "../images/food/4.jpg"
+      }
+    ],
+    noticeList: [{
+        title: "雁园点餐发布啦！"
+      },
+      {
+        title: "恭喜雁园点餐使用突破一亿次"
+      },
+      {
+        title: "恭喜贾老板喜提宝马全系"
+      }
     ]
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
-  }
+  onShareAppMessage: function() {
+
+  },
 })
