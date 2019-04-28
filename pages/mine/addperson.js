0c792cd4fe0f117413148388da01f794efaa3402
HY <2779692557@example.com>
2019-04-28 13:22:17 +0800

    HY
    0.02

--- pages/mine/addperson.js
+++ pages/mine/addperson.js
@@ -1,66 +1,105 @@
 // pages/mine/addperson.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    name: null,
+    sex: null,
+    room: null,
+    phone: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+  },
+
+  toaSaveMsg: function() {
+    app.userData.userinfo = {
+      name: this.data.name,
+      sex: this.data.sex,
+      room: this.data.room,
+      phone: this.data.phone
+    }
+    wx.showToast({
+        title: '保存成功'
+      }),
+      wx.switchTab({
+        url: '../mine/mine',
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  nameInput: function(event) {
+    this.setData({
+      name: event.detail.value
+    })
+  },
+  roomInput: function(event) {
+    this.setData({
+      room: event.detail.value
+    })
+  },
+  phoneInput: function(event) {
+    this.setData({
+      phone: event.detail.value
+    })
+  },
+  sexInput: function(event) {
+    this.setData({
+      sex: event.detail.value
+    })
   }
 })
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,18 +1,50 @@
 // pages/mine/mine.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    imageBoy:'../images/boy.png',
+    imageGirl:'../images/girl1.png',
+    selectImg:'imageGirl',
+    name:null,
+    sex:null,
+    room:null,
+    phone:null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      if (app.userData.userinfo == null){
+      wx.redirectTo({
+        url: '../mine/addperson',
+      })
+    }else{
+      this.setData({
+        name:app.userData.userinfo.name,
+        sex:app.userData.userinfo.sex,
+        room:app.userData.userinfo.room,
+        phone:app.userData.userinfo.phone,
+        })
+        if(this.data.sex = "男"){
+          this.data.selectImg = this.setData.imageBoy
+        }else{
+          this.data.selectImg = this.setData.imageGirl
+        }
+    }
+  },
 
+  reLaunch: function () {
+    wx.showToast({
+      title: 'xiugai',
+    })
+    wx.navigateTo({
+      url: '../mine/addperson'
+    })
   },
 
   /**
@@ -63,12 +95,4 @@ Page({
   onShareAppMessage: function () {
 
   },
-
-  
-  toaddperson: function () {
-    wx.navigateTo({
-      url: '../mine/addperson'
-    })
-  },
-
 })
