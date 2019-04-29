a991dfb778bbb505b81873029debe767bdcf4f1d
GMN <2779692557@qq.com>
2019-04-29 17:32:47 +0800

    HY
    我的订单页面，下单确认

--- pages/mine/myorder/myorder.js
+++ pages/mine/myorder/myorder.js
@@ -6,32 +6,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-    food:[{
-      id:"1",
+    food: [{
+      src: "../images/2.jpg",
       name: "黄焖鸡",
       price: "32元",
       num: "1份",
-    },{
-      id:"2",
-      name:"红烧狮子头",
-      price:"50元",
-      num:"3份"
-    },{
-      id:"3",
-      name:"巧克力冰淇淋",
-      price:"12元",
-      num:"1份"
-    }],
-      userName:null,
-      userSex:null,
-      userRoom:null,
-      userPhone:null
+    }, {
+        src: "../images/2.jpg",
+      name: "红烧狮子头",
+      price: "50元",
+      num: "3份"
+    }, {
+        src: "../images/2.jpg",
+      name: "巧克力冰淇淋",
+      price: "12元",
+      num: "1份"
+      }, {
+        src: "../images/2.jpg",
+        name: "蜜雪冰城",
+        price: "12元",
+        num: "9份"
+      }, {
+        src: "../images/2.jpg",
+        name: "百度音频",
+        price: "44元",
+        num: "1份"
+      }],
+    userName: null,
+    userSex: null,
+    userRoom: null,
+    userPhone: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       userName: app.userData.userinfo.name,
       userSex: app.userData.userinfo.sex,
@@ -39,53 +49,70 @@ Page({
       userPhone: app.userData.userinfo.phone,
     })
   },
-
+  toSubmitOrder:function(){
+    wx.showModal({
+      title: '订单确认',
+      content: '确认提交订单吗？',
+      confirmText:'确定',
+      cancelText:'我再想想',
+      success(res){
+        if(res.confirm){
+          console.log("确定");
+          wx.showToast({
+            title: '订单已提交',
+          })
+        }else{
+          console.log("我再想想");
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
